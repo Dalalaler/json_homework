76c8aec790bbf13fe83140bc418573f07df21f08
repatik <dalalaler@gmail.com>
2019-10-13 03:06:40 +0300

    First experience with json

--- task_1.py
+++ task_1.py
@@ -1,9 +1,17 @@
 import json
 
-with open("rooms.json", "r") as rooms_json:
+import dicttoxml
+
+rooms_path = input('Enter your full path to rooms.json: ')
+rooms_path += "/rooms.json"
+students_path = input('Enter your full path to students.json: ')
+students_path += "/students.json"
+doc_type = input('Enter formate of output file (json or xml): ')
+
+with open(rooms_path, "r") as rooms_json:
     rooms = json.load(rooms_json)
 
-with open("students.json", "r") as students_json:
+with open(students_path, "r") as students_json:
     students = json.load(students_json)
 
 stud_list = []
@@ -17,7 +25,11 @@ for room in rooms:
         'name': room['name'],
         'students': stud_list
     }
-    with open("data_file.json", "a") as write_file:
-        json.dump(dict_stud, write_file,  sort_keys=True, indent=4)
+    if doc_type == 'json':
+        with open("student_in_rooms.json", "a") as write_file:
+            json.dump(dict_stud, write_file, sort_keys=True, indent=4)
+    else:
+        with open("student_in_rooms.xml", "a") as write_file:
+            xml = dicttoxml.dicttoxml(dict_stud)
+            write_file.write(str(xml))
     stud_list.clear()
-
